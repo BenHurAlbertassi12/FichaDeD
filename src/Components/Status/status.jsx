2c56c2edaf1064db4c8d5cb080fee85dc7eb1797
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2024-04-26 21:29:47 -0300

    .

--- src/Components/Status/status.jsx
+++ src/Components/Status/status.jsx
@@ -4,22 +4,22 @@ export const Title = () => {
     return (
       <section>
         <div><p>Força</p>
-        <input type="number" />
+        <input type="number" value='forca'/>
         </div>
         <div><p>Destreza</p>
-        <input type="number" />
+        <input type="number" value='destreza'/>
         </div>
         <div><p>Constituição</p>
-        <input type="number" />
+        <input type="number" value='constutuicao'/>
         </div>
         <div><p>Inteligencia</p>
-        <input type="number" />
+        <input type="number" value='inteligencia'/>
         </div>
         <div><p>Sabedoria</p>
-        <input type="number" />
+        <input type="number" value='sabedoria'/>
         </div>
         <div><p>Carisma</p>
-        <input type="number" />
+        <input type="number" value='carisma'/>
         </div>
       </section>
     );
