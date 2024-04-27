50d75e43beb795cd03318215e77f4643fd66bbd9
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2024-04-26 21:41:59 -0300

    .

--- src/Components/Status/status.jsx
+++ src/Components/Status/status.jsx
@@ -1,44 +1,64 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-export const Status = () => { 
-    const [forca, setForca] = useState('');
-    const [destreza, setdestreza] = useState('');
-    const [constutuicao, setconstutuicao] = useState('');
-    const [inteligencia, setinteligencia] = useState('');
-    const [sabedoria, setsabedoria] = useState('');
-    const [carisma, setcarisma] = useState('');
-    
-    return (
-      <section>
-        <div>
-          <p>Força</p>
-          <input
-            type='number'
-            value={forca}
-            onChange={(e) => setForca(e.target.value)}
-          />
-        </div>
+export const Status = () => {
+  const [forca, setForca] = useState('');
+  const [destreza, setdestreza] = useState('');
+  const [constutuicao, setconstutuicao] = useState('');
+  const [inteligencia, setinteligencia] = useState('');
+  const [sabedoria, setsabedoria] = useState('');
+  const [carisma, setcarisma] = useState('');
 
-        <div>
-          <p>Destreza</p>
-          <input type='number' value='destreza' />
-        </div>
-        <div>
-          <p>Constituição</p>
-          <input type='number' value='constutuicao' />
-        </div>
-        <div>
-          <p>Inteligencia</p>
-          <input type='number' value='inteligencia' />
-        </div>
-        <div>
-          <p>Sabedoria</p>
-          <input type='number' value='sabedoria' />
-        </div>
-        <div>
-          <p>Carisma</p>
-          <input type='number' value='carisma' />
-        </div>
-      </section>
-    );
-}
+  return (
+    <section>
+      <div>
+        <p>Força</p>
+        <input
+          type='number'
+          value={forca}
+          onChange={(e) => setForca(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <p>Destreza</p>
+        <input
+          type='number'
+          value={destreza}
+          onChange={(e) => setdestreza(e.target.value)}
+        />
+      </div>
+      <div>
+        <p>Constituição</p>
+        <input
+          type='number'
+          value={constutuicao}
+          onChange={(e) => setconstutuicao(e.target.value)}
+        />
+      </div>
+      <div>
+        <p>Inteligencia</p>
+        <input
+          type='number'
+          value={inteligencia}
+          onChange={(e) => setinteligencia(e.target.value)}
+        />
+      </div>
+      <div>
+        <p>Sabedoria</p>
+        <input
+          type='number'
+          value={sabedoria}
+          onChange={(e) => setsabedoria(e.target.value)}
+        />
+      </div>
+      <div>
+        <p>Carisma</p>
+        <input
+          type='number'
+          value={carisma}
+          onChange={(e) => setcarisma(e.target.value)}
+        />
+      </div>
+    </section>
+  );
+};
