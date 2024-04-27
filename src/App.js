6ed5ee1228cb2a1e6dd37ab8ed4f2d92d2310ad4
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2024-04-26 21:42:57 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { Status } from './Components/Status/status';
-// import { Title } from './Components/Title/title';
+import { Title } from './Components/Title/title';
 
 function App() {
   return (
     <div className="App">
-      {/* <Title></Title> */}
+      <Title></Title>
       <Status></Status>
     </div>
   );
