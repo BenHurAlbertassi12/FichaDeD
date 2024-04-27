2c927ca2ba03005d2e061e760dbb8cd40c96115b
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2024-04-26 21:28:01 -0300

    .

--- src/Components/Status/status.jsx
+++ src/Components/Status/status.jsx
@@ -3,12 +3,24 @@ import React from "react";
 export const Title = () => { 
     return (
       <section>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div><p>Força</p>
+        <input type="number" />
+        </div>
+        <div><p>Destreza</p>
+        <input type="number" />
+        </div>
+        <div><p>Constituição</p>
+        <input type="number" />
+        </div>
+        <div><p>Inteligencia</p>
+        <input type="number" />
+        </div>
+        <div><p>Sabedoria</p>
+        <input type="number" />
+        </div>
+        <div><p>Carisma</p>
+        <input type="number" />
+        </div>
       </section>
     );
 }
